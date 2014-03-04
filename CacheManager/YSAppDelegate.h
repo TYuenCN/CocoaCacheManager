ca7806bfc9c8d393753546fff1ba5519d7725148
Yuan Zheng <yuanzheng@sxzw.tv>
2014-03-04 13:39:28 +0800

    Initial Commit

---
+++ CacheManager/YSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YSAppDelegate.h
+//  CacheManager
+//
+//  Created by sxzw on 14-3-4.
+//  Copyright (c) 2014年 YuenSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CacheManagerTests/CacheManagerTests.m
@@ -0,0 +1,34 @@
+//
+//  CacheManagerTests.m
+//  CacheManagerTests
+//
+//  Created by sxzw on 14-3-4.
+//  Copyright (c) 2014年 YuenSoft. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CacheManagerTests : XCTestCase
+
+@end
+
+@implementation CacheManagerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
